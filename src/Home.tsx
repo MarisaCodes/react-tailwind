1b5d06800d130d240e9d91f23ad5bd4e453e4c1d
MarisaCodes <marisakirisame955@gmail.com>
2023-08-31 07:57:37 +0400

    fleshed out navbar style

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,7 @@ import ToggleTheme from "./components/ToggleTheme";
 import { ThemeContext } from "./context/ThemeContext";
 import GradientButton from "./components/GradientButton";
 import CodeBlock from "./components/CodeBlock";
+import reactLogo from "./assets/react.svg";
 
 const Home = () => {
   const [mode, setMode] = useState("dark");
@@ -53,31 +54,26 @@ const Home = () => {
                 mode using the button above.
               </p>
               <p className="mt-2">
-                <a
-                  href="#"
-                  className="hover:underline text-blue-400 dark:text-blue-600"
-                >
+                <a href="#" className="link">
                   Learn more
                 </a>
               </p>
             </div>
             <div>
-              <h2 className="text-3xl pb-5 font-semibold dark:text-white underline underline-offset-4 w-full">
-                Toggle Theme:
-              </h2>
+              <h2 className="subheading">Toggle Theme</h2>
               <ToggleTheme mode={mode} setMode={setMode} />
               <div className="flex flex-col">
                 <CodeBlock codeEls={codeEls} code={toggleThemeCode} />
                 <div className="pt-5 pb-4 flex flex-col gap-5">
                   <a
-                    className="hover:underline text-blue-400 dark:text-blue-600 w-fit"
+                    className="link"
                     href="https://tailwindcss.com/docs/dark-mode#toggling-dark-mode-manually"
                   >
                     Tailwind css dark mode `class` strategy
                   </a>
                   <a
                     href="https://react.dev/learn/passing-data-deeply-with-context"
-                    className="hover:underline text-blue-400 dark:text-blue-600 w-fit"
+                    className="link"
                   >
                     Learn React Context
                   </a>
@@ -92,9 +88,7 @@ const Home = () => {
               </div>
             </div>
             <div className="flex gap-5 flex-wrap items-center justify-center md:justify-normal">
-              <h2 className="text-3xl pb-5 font-semibold dark:text-white underline underline-offset-4 w-full">
-                Gradient Buttons:
-              </h2>
+              <h2 className="subheading">Gradient Button</h2>
               <GradientButton from="from-emerald-300" to="to-blue-500">
                 Sign Up
               </GradientButton>
@@ -119,6 +113,63 @@ const Home = () => {
                 </GradientButton>
               </div>
             </div>
+            <div>
+              <h2 className="subheading">Navbar</h2>
+              <nav className="flex items-center justify-between pr-0 sm:px-2 md:px-1 md:justify-normal dark:bg-gray-800 dark:rounded-lg h-14 dark:border-0 border-b border-gray-600">
+                <div className="p-2">
+                  <a
+                    href="https://react.dev"
+                    className="flex gap-2 items-center"
+                    target="_blank"
+                  >
+                    <img src={reactLogo} alt="" />
+                    <span className="dark:text-white text-lg">React</span>
+                  </a>
+                </div>
+                <div className="flex-grow h-full block md:hidden">
+                  <div className="h-full ml-auto w-fit px-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-r-lg cursor-pointer flex justify-center flex-col gap-2">
+                    <div className="h-0.5 w-8 rounded-full dark:bg-gray-300 bg-gray-800"></div>
+                    <div className="h-0.5 w-8 rounded-full dark:bg-gray-300 bg-gray-800"></div>
+                    <div className="h-0.5 w-8 rounded-full dark:bg-gray-300 bg-gray-800"></div>
+                  </div>
+                </div>
+                <div className="lg:flex-grow flex items-center relative h-full md:ml-5">
+                  <label
+                    htmlFor="more"
+                    className="h-full items-center hidden lg:hidden md:flex cursor-pointer"
+                  >
+                    More &#x25BC;
+                  </label>
+                  <input
+                    type="checkbox"
+                    name="more"
+                    id="more"
+                    className="hidden peer"
+                  />
+                  <div className="lg:flex-grow lg:flex justify-center lg:gap-14 hidden peer-checked:bg-white z-10 peer-checked:border-2 peer-checked:border-gray-400 peer-checked:dark:bg-gray-700 peer-checked:dark:text-white peer-checked:p-4 peer-checked:rounded-lg lg:border-0 peer-checked:flex-col peer-checked:gap-3 sm:peer-checked:flex peer-checked:absolute peer-checked:top-10">
+                    <a href="#" className="link">
+                      About
+                    </a>
+                    <a href="#" className="link">
+                      Services
+                    </a>
+                    <a href="#" className="link">
+                      Contact
+                    </a>
+                  </div>
+                </div>
+
+                <div className="hidden md:flex lg:flex-grow-0 md:flex-grow md:justify-end items-center lg:gap-5 md:gap-2">
+                  <GradientButton from="from-red-500" to="to-yellow-500">
+                    Sign Up
+                  </GradientButton>
+                  <GradientButton from="from-blue-500" to="to-green-500">
+                    Login
+                  </GradientButton>
+                </div>
+              </nav>
+            </div>
+            <div className="pt-10"></div>
           </main>
         </div>
       </div>
